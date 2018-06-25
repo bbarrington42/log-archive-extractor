523ce9a796d78cf900afe0f0e80da4f0f77c9c80
Bill Barrington <bill.barrington@candidpartners.com>
2018-06-25 10:29:26 -0400

    Created method to unzip S3 Object content and close the underlying stream.

--- src/main/scala/com/util/S3ObjectIterator.scala
+++ src/main/scala/com/util/S3ObjectIterator.scala
@@ -1,13 +1,19 @@
 package com.util
 
+import java.io.{BufferedReader, InputStreamReader}
+import java.util.function.BinaryOperator
+import java.util.zip.GZIPInputStream
+
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import com.amazonaws.services.s3.model.{ObjectListing, S3Object}
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 
 import scala.collection.JavaConverters._
 
 /*
   An iterator for a collection of objects in S3. This handles truncated lists.
+  Note that an S3Object is returned from the iterator and this object must be closed after use or a resource leak
+  will occur.
  */
 class S3ObjectIterator(s3: AmazonS3, objectListing: ObjectListing) extends Iterator[S3Object] {
 
@@ -28,6 +34,7 @@ class S3ObjectIterator(s3: AmazonS3, objectListing: ObjectListing) extends Itera
     private val summaries = objectListing.getObjectSummaries.asScala
     private val bucketName = objectListing.getBucketName
 
+    println(s"Got ${summaries.length} summaries...")
 
     override def hasNext: Boolean = index < objectListing.getObjectSummaries.size
 
@@ -41,7 +48,22 @@ class S3ObjectIterator(s3: AmazonS3, objectListing: ObjectListing) extends Itera
 
 }
 
+object S3ObjectIterator {
+  // Retrieve object as a String and close the underlying HTTP connection.
+  def getContentAsString(s3Object: S3Object): String = try {
+    val op = new BinaryOperator[String] {
+      override def apply(t: String, u: String): String = t + u
+    }
+    val br = new BufferedReader(new InputStreamReader(new GZIPInputStream(s3Object.getObjectContent)))
+    br.lines().reduce("", op)
+  } finally {
+    s3Object.close()
+  }
+}
+
 object Main {
+  import S3ObjectIterator._
+  
   def main(args: Array[String]): Unit = {
 
     val credentialsProvider = new ProfileCredentialsProvider("cda")
@@ -55,9 +77,14 @@ object Main {
 
     val iter = new S3ObjectIterator(s3, objectListing)
 
-    while(iter.hasNext) {
-      val next = iter.next()
-      println(next)
-    }
+    iter.foreach(s3Object => println(getContentAsString(s3Object)))
+//
+//    var i = 1
+//    while (iter.hasNext) {
+//      val next = iter.next()
+//      val r = getContentAsString(next)
+//
+//      println(r)
+//    }
   }
 }
